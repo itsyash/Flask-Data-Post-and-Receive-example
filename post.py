9f15c9764f7fd964ddff558423320781c2c19a8b
yash <yash.girdhar@gmail.com>
2015-03-24 00:10:39 +0530

    added .gitignore

--- post.py
+++ post.py
@@ -1,6 +1,6 @@
 import urllib, urllib2
 
-url = 'http://127.0.0.1:5000/welcome'
+url = 'http://localhost:5000/welcome'
 args = {'name' : 'yash', 'email' : 'yash.girdhar@gmail.com'}
 encoded_args = urllib.urlencode(args)
 response = urllib2.urlopen(url,encoded_args).read()
